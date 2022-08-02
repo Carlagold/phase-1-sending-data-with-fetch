51ed47906cf5a732227287247363c09a77169ccd
Carla <csachett@student.scf.edu>
2022-08-02 14:21:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+ // Add your code here
+ function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { 
+      "Content-Type": "application/json",
+       Accept: "application/json"
+    },
+    body: JSON.stringify({name, email}),
+   
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (json) {
+  //  console.log(json); 
+   // Console.log on browser console to see the newly assigned id.
+      const body = document.querySelector("body");
+      document.body.append(json.id)    
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      document.body.append(error.message)
+   
+    })
+}
+submitData("Steve", "steve@steve.com");
+submitData("Sam", "sam@sam.com");
+submitData("Jim", "jim@jim.com");
+
